fbf4c4191a4a847d380c8c480a893efdc5068a82
Adil Shamshad <adil.shamshad@liveramp.com>
2020-12-09 16:22:10 +0000

    Adding a unit test alongside the CommandLineHelper

--- packager/src/main/java/io/cdap/hub/Tool.java
+++ packager/src/main/java/io/cdap/hub/Tool.java
@@ -21,12 +21,10 @@ import com.google.common.io.BaseEncoding;
 import com.google.common.primitives.Longs;
 import io.cdap.hub.publisher.Publisher;
 import io.cdap.hub.publisher.PublisherFactory;
-import io.cdap.hub.publisher.S3Publisher;
 import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -150,60 +148,6 @@ public class Tool {
     }
 
     publisher.publish(hub);
-  }
-
-  private static S3Publisher getPublisher(CommandLine commandLine, Set<String> whitelist) {
-
-    if (!commandLine.hasOption("s3b")) {
-      LOG.error("Must specify a bucket when publishing.");
-      System.exit(1);
-    }
-    String bucket = commandLine.getOptionValue("s3b");
-
-    if (!commandLine.hasOption("s3a")) {
-      LOG.error("Must specify an s3 access key when publishing.");
-      System.exit(1);
-    }
-    String s3AccessKey = commandLine.getOptionValue("s3a");
-
-    if (!commandLine.hasOption("s3s")) {
-      LOG.error("Must specify an s3 secret key when publishing.");
-      System.exit(1);
-    }
-    String s3SecretKey = commandLine.getOptionValue("s3s");
-
-    S3Publisher.Builder builder = S3Publisher.builder(bucket, s3AccessKey, s3SecretKey)
-      .setForcePush(commandLine.hasOption('f'))
-      .setDryRun(commandLine.hasOption('y'))
-      .setWhitelist(whitelist);
-
-    // default to 'v2'
-    String version = commandLine.hasOption("v") ? commandLine.getOptionValue("v") : "v2";
-
-    if (commandLine.hasOption("s3p")) {
-      String prefix = commandLine.getOptionValue("s3p");
-      builder.setPrefix(prefix.endsWith("/") || prefix.isEmpty() ? prefix + version : prefix + "/" + version);
-    } else {
-      builder.setPrefix(version);
-    }
-
-    if (commandLine.hasOption("s3t")) {
-      builder.setTimeout(Integer.parseInt(commandLine.getOptionValue("s3t")));
-    }
-
-    if (commandLine.hasOption("cfa")) {
-      builder.setCloudfrontAccessKey(commandLine.getOptionValue("cfa"));
-    }
-
-    if (commandLine.hasOption("cfs")) {
-      builder.setCloudfrontSecretKey(commandLine.getOptionValue("cfs"));
-    }
-
-    if (commandLine.hasOption("cfd")) {
-      builder.setCloudfrontDistribution(commandLine.getOptionValue("cfd"));
-    }
-
-    return builder.build();
   }
 
   private static Set<String> parseWhitelist(String whitelistStr) {
